57d8e1be633cb03612e0cb6f10e7aacdaf6e9e7f
Garet C. Green <garetgreen1996@gmail.com>
2018-02-15 10:33:52 -0600

    Initial upload

---
+++ feedback-thanks.php
@@ -0,0 +1,9 @@
+<title>Thank You for Your Review of COMPANY</title>
+<meta name="description" itemprop="description" property="og:description" content="Thank you for your review of COMPANY. We appreciate your feedback on the work our local KEYPHRASE provided.">
+<meta name="keywords" content="feedback, review, LOWERKEY">
+
+<h1>Thank You for Your Feedback</h1>
+
+<p>Thank you for reviewing the services you received from <strong>COMPANY</strong>. We appreciate your business and want you to know we value your opinion of the service you received. Should you have any concerns about your project or the quality of service our <a href="<?= $baseUrl ?>">KEYPHRASE</a> company provided, don't hesitate to reach out to us by calling us at <?= $phoneNumber ?>.</p>
+
+<h3>If you have concerns about the quality of service our KEYPHRASE provided, please call <?= $phoneNumber ?> or complete our <a href="<?= $baseUrl ?>/estimate.php">online request form</a>.</h3>
---
+++ newsletter-thanks.php
@@ -0,0 +1,9 @@
+<title>Thank You for Signing Up | SERVICE News</title>
+<meta name="description" itemprop="description" property="og:description" content="Thank you for signing up for news from your local KEYPHRASE via COMPANY's newsletter.">
+<meta name="keywords" content="newsletter, LOWERKEY, LOWERCOMP">
+
+<h1>Thank You for Signing Up</h1>
+
+<p>Thank you for signing up to receive news from <strong>COMPANY</strong>. You should receive your first newsletter, filled with company news, <a href="<?= $baseUrl ?>">SERVICE</a> tips, exclusive service specials and more within the following month.</p>
+
+<h3>If you would like the latest news from your local KEYPHRASE, please call <?= $phoneNumber ?> or complete our <a href="<?= $baseUrl ?>/estimate.php">online request form</a></h3>
